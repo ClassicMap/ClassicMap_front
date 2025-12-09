629c9f21cb9218fdc5b5dbad25c6b006a892f79d
KDH <kdh@gangdonghyeons-Mac-mini.local>
2025-12-09 21:38:29 +0900

    fix

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,10 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+// Set public path for web builds
+config.transformer = {
+  ...config.transformer,
+  publicPath: '/classicmap',
+};
+
 module.exports = withNativeWind(config, { input: './global.css', inlineRem: 16 });
