a90059ea15fe76db47bb4da3b72c6cc7f0dd1e95
KDH <kdh@gangdonghyeons-Mac-mini.local>
2025-12-09 16:57:20 +0900

    fix

--- app.config.js
+++ app.config.js
@@ -2,69 +2,57 @@ import 'dotenv/config';
 
 export default {
   expo: {
-    name: "클래식맵",
-    slug: "ClassicMap_front",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/images/icon.png",
-    scheme: "classicmap-front",
-    userInterfaceStyle: "automatic",
+    name: '클래식맵',
+    slug: 'ClassicMap_front',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/images/icon.png',
+    scheme: 'classicmap-front',
+    userInterfaceStyle: 'automatic',
     newArchEnabled: true,
     splash: {
-      image: "./assets/images/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff"
+      image: './assets/images/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
     },
-    assetBundlePatterns: [
-      "**/*"
-    ],
+    assetBundlePatterns: ['**/*'],
     ios: {
       supportsTablet: true,
-      bundleIdentifier: "com.kang1027.classicmap",
-      buildNumber: "1",
+      bundleIdentifier: 'com.kang1027.classicmap',
+      buildNumber: '1',
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
         NSAppTransportSecurity: {
           NSAllowsArbitraryLoads: true,
-          NSExceptionDomains: {
-            "34.60.221.92": {
-              NSExceptionAllowsInsecureHTTPLoads: true,
-              NSIncludesSubdomains: true
-            }
-          }
-        }
-      }
+        },
+      },
     },
     android: {
       edgeToEdgeEnabled: true,
       adaptiveIcon: {
-        foregroundImage: "./assets/images/adaptive-icon.png",
-        backgroundColor: "#ffffff"
+        foregroundImage: './assets/images/adaptive-icon.png',
+        backgroundColor: '#ffffff',
       },
-      package: "com.kang1027.classicmap",
-      usesCleartextTraffic: true
+      package: 'com.kang1027.classicmap',
+      usesCleartextTraffic: true,
     },
     web: {
-      bundler: "metro",
-      output: "static",
-      favicon: "./assets/images/favicon.png"
+      bundler: 'metro',
+      output: 'static',
+      favicon: './assets/images/favicon.png',
     },
-    plugins: [
-      "expo-router",
-      "expo-secure-store",
-      "expo-web-browser"
-    ],
+    plugins: ['expo-router', 'expo-secure-store', 'expo-web-browser'],
     experiments: {
-      typedRoutes: true
+      typedRoutes: true,
     },
     extra: {
       router: {},
       eas: {
-        projectId: "0a9c2f70-6f6f-4729-a934-d10b39d89833"
+        projectId: '0a9c2f70-6f6f-4729-a934-d10b39d89833',
       },
       // 환경 변수를 명시적으로 extra에 포함
       clerkPublishableKey: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
       apiUrl: process.env.EXPO_PUBLIC_API_URL,
-    }
-  }
+    },
+  },
 };
